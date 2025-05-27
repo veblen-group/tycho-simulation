3955924b08cef4cc70bd98db68d797d00003f0cf
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2025-05-27 13:07:17 -0400

    fix: formatting

--- examples/quickstart/main.rs
+++ examples/quickstart/main.rs
@@ -183,14 +183,13 @@ async fn main() {
                     tvl_filter.clone(),
                     Some(uniswap_v4_pool_with_hook_filter),
                 )
-                .exchange::<EkuboState>("ekubo_v2", tvl_filter.clone(), None);
+                .exchange::<EkuboState>("ekubo_v2", tvl_filter.clone(), None)
                 .exchange::<EVMPoolState<PreCachedDB>>(
                     "vm:curve",
                     tvl_filter.clone(),
                     Some(curve_pool_filter),
                 )
-                .exchange::<EVMPoolState<PreCachedDB>>("vm:maverick_v2", tvl_filter
-                .clone(), None);
+                .exchange::<EVMPoolState<PreCachedDB>>("vm:maverick_v2", tvl_filter.clone(), None);
         }
         Chain::Base => {
             protocol_stream = protocol_stream
